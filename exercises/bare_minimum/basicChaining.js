0560c3e61e1f772d70cb5a9c8e210c293871232d
Manuela Gomez <mmanu.gomez@gmail.com>
2019-09-26 21:55:58 -0400

    Complete Bare Minimum Requirements

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,25 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var pluckFirstLineFromFileAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
+var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync;
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  // (1) reads a GitHub username from a `readFilePath`
+  //     (the username will be the first line of the file)
+  return pluckFirstLineFromFileAsync(readFilePath)
+    // (2) then, sends a request to the GitHub API for the user's profile
+    .then( (username) => {
+      return getGitHubProfileAsync(username);
+    })
+    // (3) then, writes the JSON response of the API to `writeFilePath`
+    .then( (body) => {
+      var fileData = JSON.stringify(body);
+      fs.writeFile(writeFilePath, fileData, 'utf8', (error) => {
+        console.log(fileData);
+        if (error) { throw error; }
+      });
+    });
 };
 
 // Export these functions so we can test them
